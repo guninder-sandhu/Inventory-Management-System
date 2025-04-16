0e92aa78ddd44037d824bf69c92c4b2fb32a1da0
gsandhu <gunisandhu@gmail.com>
2025-04-16 13:36:36 +1000

    setup: Created user-service module

--- user-service/src/test/java/com/inventory/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/inventory/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.inventory.management;
+package com.inventory.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class InventoryManagementSystemApplicationTests {
+class UserServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
