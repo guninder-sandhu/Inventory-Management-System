95e57d2cb41c9f6d5d583c2003f6067e2c840cb8
guninder sandhu <guninder.sandhu@objective.com>
2025-05-25 22:37:57 +1000

    All microservices dockerized

--- product/entrypoint.sh
+++ product/entrypoint.sh
@@ -3,5 +3,6 @@
 /wait-for-it.sh postgres:5432 --timeout=60
 /wait-for-it.sh configserver:8084 --timeout=60
 /wait-for-it.sh service-registry:8761 --timeout=60
+/wait-for-it.sh stock-service:8083 --timeout=90
 
 exec java -jar /app/app.jar
